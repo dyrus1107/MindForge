8968a6a0094142987c6e5c52f9619058f64b6044
silverian <dyrus1107@gmail.com>
2024-04-28 10:38:10 +0700

    fix some issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { EdgeStoreProvider } from "@/lib/edgestore";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mind Forge",
+  title: "MindForge",
   description: "The connected workspace where better, faster work happens",
   icons: {
     icon: [
@@ -44,7 +44,7 @@ export default function RootLayout({
               defaultTheme="system"
               enableSystem
               disableTransitionOnChange
-              storageKey="jotion-theme"
+              storageKey="mindforge-theme"
             >
               <Toaster position="bottom-center" />
               <ModalProvider />
